2ea908aad9111b8a62a4fe0d1d71e5dd69c03c4d
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-05-21 05:22:54 +0200

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'), 
+sass = require('gulp-sass') 
+notify = require("gulp-notify") 
+bower = require('gulp-bower')
+copy = require("gulp-copy");
+
+
+var config = {
+     sassPath: "./styles",
+     bowerDir: "./bower_components" ,
+    wwwroot: "./wwwroot"
+}
+
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+gulp.task('copy-fonts', function() { 
+    return gulp.src([config.bowerDir + '/font-awesome/fonts/**.*',
+                     config.bowerDir + '/bootstrap/dist/fonts/**.*']) 
+    .pipe(gulp.dest(config.wwwroot +'/fonts')); 
+});
+
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/style.scss')
+        .pipe(sass({
+            includePaths: [config.bowerDir + 'bootstrap-sass-official/assets/stylesheets'],
+        }))
+        .pipe(gulp.dest(config.wwwroot + '/css'));
+});
+
+
+
+gulp.task('copy-css', function() { 
+    return gulp.src([config.bowerDir + "/bootstrap/dist/css/bootstrap.css",
+                     config.bowerDir + "/bootstrap/dist/css/bootstrap-theme.css"])
+        .pipe(gulp.dest(config.wwwroot + "/css")); 
+});
+
+gulp.task('copy-js', function() { 
+    return gulp.src([
+        config.bowerDir + "/bootstrap/dist/js/bootstrap.js",
+        config.bowerDir + "/angular/angular.js",
+        config.bowerDir + "/angular/angular.min.js.map",
+        config.bowerDir + "/angular-loader/angular-loader.js",
+        config.bowerDir + "/angular-loader/angular-loader.min.js.map",
+        config.bowerDir + "/angular-mocks/angular-mocks.js",
+        config.bowerDir + "/angular-route/angular-route.js",
+        config.bowerDir + "/angular-route/angular-route.min.js.map",
+
+    ])
+        .pipe(gulp.dest(config.wwwroot + "/js")); 
+});
+
+  gulp.task('copy', ['copy-fonts', 'copy-css', 'copy-js']);
+  gulp.task('default', ['bower', 'copy']);
